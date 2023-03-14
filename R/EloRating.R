a42b55a20da4e4f5a58bbb7e3c906a97edfce027
Jeffreyjose29 <jeffreyjose91@gmail.com>
2023-03-14 17:59:18 +1300

    Completed Pep Managerial Career Elo Rating Based

--- R/EloRating.R
+++ R/EloRating.R
@@ -1,11 +1,5 @@
 library(ggplot2); library(StatsBombR); library(dplyr); library(tidyverse)
 
-freeCompetitions <- FreeCompetitions() %>%
-  filter(competition_id == 1238)
-freeMatches <- FreeMatches(freeCompetitions)
-matchEvents <- free_allevents(MatchesDF = freeMatches, Parallel = T)
-  
-
 ## Club Elo-Rating - Pep Guardiola
 setwd("C:/Users/jeffr/OneDrive/Desktop/Github Activities/ClubSoccerPredictions/Data")
 barcelona <- read.csv("barcelona_elo_rating.csv", header = TRUE)
@@ -42,15 +36,38 @@ elo_rating %>%
   labs(y = "Club Elo Rating",
        x = "",
        title = "Pep Guardiola's Managerial History",
-       subtitle = "After creating a name for himself with a mega team in Barcelona and conquering the La Liga, Guardiola has given FC Bayern Munich & Manchester City FC the experience and masterclass to extend their dominance in the Bundesliga & English Premier League.") +
+       subtitle = "After creating a name for himself with a mega team in Barcelona and conquering the La Liga, Guardiola has given FC Bayern Munich & Manchester City FC the experience and masterclass to extend their dominance in the Bundesliga & English Premier League.",
+       caption = "Created By: @dataden_\nSource: clubelo.com") +
   theme(legend.position = "none", axis.title.y = element_text(size = 14, colour = "#131313"),
         axis.text = element_text(size = 12, colour = "#333232"),
         plot.title = element_text(size = 22, hjust = 0.5, face = "bold", colour = "#161616"),
-        plot.subtitle = element_text(size = 16, hjust = 0.5, colour = "#131313")) + 
+        plot.subtitle = element_text(size = 16, hjust = 0.5, colour = "#131313"),
+        plot.caption = element_text(colour = "#131313", size = 9, hjust = 0.5)) + 
   scale_y_continuous(limits = c(1800, 2200)) +
   annotate("segment", x = as.Date("2008-07-01"), xend = as.Date("2012-06-30"), y = 2175, yend = 2175, colour = "#a60042", size = 2) +
   annotate("segment", x = as.Date("2013-07-01"), xend = as.Date("2016-06-30"), y = 2175, yend = 2175, colour = "#dd0129", size = 2) +
-  annotate("segment", x = as.Date("2016-07-01"), xend = as.Date(max(elo_rating$From)), y = 2175, yend = 2175, colour = "#6cafe0", size = 2) 
+  annotate("segment", x = as.Date("2016-07-01"), xend = as.Date(max(elo_rating$From)), y = 2175, yend = 2175, colour = "#6cafe0", size = 2) +
+  geom_text(aes(x = as.Date("2010-07-01"), y = 2183, label = "FC Barcelona"), colour = "#a60042", size = 7) +
+  geom_text(aes(x = as.Date("2015-01-01"), y = 2183, label = "FC Bayern München"), colour = "#dd0129", size = 7) +
+  geom_text(aes(x = as.Date("2020-01-01"), y = 2183, label = "Manchester City FC"), colour = "#6cafe0", size = 7) +
+  ## Label Annotation
+  ### Barcelona FC
+  geom_label(aes(x = as.Date("2009-05-13"), y = 1900 , label = "Copa del Rey"), colour = "#131313") +
+  geom_label(aes(x = as.Date("2009-05-27"), y = 2025, label = "UEFA Champions League"), colour = "#131313") +
+  geom_label(aes(x = as.Date("2011-05-28"), y = 1980, label = "UEFA Champions League"), colour = "#131313") +
+  geom_label(aes(x = as.Date("2011-08-17"), y = 2020, label = "Supercopa"), colour = "#131313") +
+  geom_label(aes(x = as.Date("2011-12-18"), y = 2125, label = "FIFA Club World Cup"), colour = "#131313") +
+  geom_label(aes(x = as.Date("2012-05-25"), y = 2025, label = "Copa del Rey"), colour = "#131313") +
+  ### FC Bayern Munich
+  geom_label(aes(x = as.Date("2013-08-30"), y = 2000, label = "UEFA Super Cup"), colour = "#131313") +
+  geom_label(aes(x = as.Date("2014-05-17"), y = 2120, label = "DFB-Pokal"), colour = "#131313") +
+  geom_label(aes(x = as.Date("2016-05-04"), y = 2100, label = "UEFA Champions League Semi-Final"), colour = "#131313") +
+  geom_label(aes(x = as.Date("2016-05-21"), y = 1975, label = "DFB-Pokal"), colour = "#131313") +
+  ### Manchester City
+  geom_label(aes(x = as.Date("2018-04-15"), y = 2050, label = "Premier League Champions (100 Points)"), colour = "#131313") +
+  geom_label(aes(x = as.Date("2019-05-18"), y = 1940, label = "FA Cup"), colour = "#131313") +
+  geom_label(aes(x = as.Date("2021-05-29"), y = 2070, label = "UEFA Champions League Final"), colour = "#131313")
+
 
 
 ## ARSENAL ELO-RATING
